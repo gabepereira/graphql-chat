f6ac9421257968aa9972da2c5eb82a8983456739
gabepereira <gabriel6562014@gmail.com>
2019-09-09 04:01:45 -0300

    Finished basic functions

--- src/resolvers/Room.js
+++ src/resolvers/Room.js
@@ -19,9 +19,15 @@ const Mutation = {
         err ? new Error('Error updating user: ') : doc);
         return room ? room : new Error('Failed to create room.');
     },
-    // joinRoom: () => {
-
-    // }
+    joinRoom: async(_, { room }, ctx, info) => {
+        const user = await User.findById(ctx.token.id);
+        const _room = await Room.findById(room);
+        if (!_room) throw new Error('No room found.');
+        user.rooms.push(_room.id);
+        user.updateOne(user, (err, doc) => 
+        err ? new Error('Error updating user: ') : doc);
+        return _room ? _room : new Error('Failed to join room.');
+    }
 };
 
 module.exports = {
